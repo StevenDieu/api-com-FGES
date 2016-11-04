340db4aa160c1f0a311c1b84e8a7c69440c54c78
Steven Dieu <groover.Dieu@gmail.com>
2016-11-04 08:42:34 +0100

    Update url api

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -2,39 +2,71 @@
 
 class ApiController extends Controller {
 
-    public function flashback($id = null) {
+    public function flashback($idOrPage = null, $start = null) {
         header('Content-type: text/plain');
         header("Access-Control-Allow-Origin: *");
-        if ($id) {
-            $flashbackConstruct = new Flashback();
-            $flashbackConstruct->setId($id);
-            $flashback = $flashbackConstruct->getFlashbackByIdActive();
-            if (!empty($flashback)) {
-                $jsonFlashback["titre"] = $flashback["titre"];
-                new DateTime($flashback["date_debut"]);
-                date_default_timezone_set('Europe/Paris');
-                setlocale(LC_TIME, 'fr_FR.utf8', 'fra'); // OK
-                $jsonFlashback["date_debut"] = strftime("%A %d %B %Y");
-                $jsonFlashback["description"] = $flashback["description"];
-                echo json_encode($jsonFlashback);
+        if ($idOrPage != null) {
+            if ($idOrPage == "page") {
+                $this->getListe("flashback", $start, $idOrPage);
+            } else {
+                $flashbackConstruct = new Flashback();
+                $flashbackConstruct->setId($idOrPage);
+                $flashback = $flashbackConstruct->getFlashbackByIdActive();
+                if (!empty($flashback)) {
+                    $jsonFlashback["titre"] = $flashback["titre"];
+                    new DateTime($flashback["date_debut"]);
+                    date_default_timezone_set('Europe/Paris');
+                    setlocale(LC_TIME, 'fr_FR.utf8', 'fra'); // OK
+                    $jsonFlashback["date_debut"] = strftime("%A %d %B %Y");
+                    $jsonFlashback["description"] = $flashback["description"];
+                    echo json_encode($jsonFlashback);
+                }
             }
         }
     }
 
-    public function photo($id_album = null, $id = null) {
+    public function photo($idAlbum = null, $idOrPage = null, $start = null) {
         header('Content-type: text/plain');
         header("Access-Control-Allow-Origin: *");
-        if ($id != null && $id_album != null) {
+        if ($idOrPage != null && $idAlbum != null) {
+            if ($idOrPage == "page") {
+                $this->listPhoto($idAlbum, $start);
+            } else {
+                $photoConstruct = new Photos();
+                $photoConstruct->setId($idOrPage);
+                $photoConstruct->setId_album($idAlbum);
+                $photo = $photoConstruct->getPhotoById();
+                if (!empty($photo)) {
+                    $jsonPhoto["name"] = $photo["name"];
+                    $jsonPhoto["id"] = $photo["id"];
+                    $photo["url"] = str_replace("\\", "/", $photo["url"]);
+                    $jsonPhoto["url"] = str_replace('/assets/img/', 'http://' . $_SERVER['HTTP_HOST'] . '/assets/img/', $photo["url"]);
+                    echo json_encode($jsonPhoto);
+                }
+            }
+        }
+    }
+
+    private function listPhoto($idAlbum = null, $start = null) {
+        if ($start != null) {
+            $json = array();
+            $jsonPhotos = array();
             $photoConstruct = new Photos();
-            $photoConstruct->setId($id);
-            $photoConstruct->setId_album($id_album);
-            $photo = $photoConstruct->getPhotoById();
-            if (!empty($photo)) {
-                $jsonPhoto["name"] = $photo["name"];
-                $jsonPhoto["id"] = $photo["id"];
-                $photo["url"] = str_replace("\\", "/", $photo["url"]);
-                $jsonPhoto["url"] = str_replace('/assets/img/', 'http://' . $_SERVER['HTTP_HOST'] . '/assets/img/', $photo["url"]);
-                echo json_encode($jsonPhoto);
+            $photoConstruct->setId_album($idAlbum);
+            $count = $photoConstruct->countPhotosByIdAlbum();
+            if ($count > 0 && ($start == 0 || $count - $start > 0)) {
+                foreach ($photoConstruct->getAllPhotosByPageAndIdAlbum($start) as $photos) {
+                    $jsonPhoto["id"] = $photos["id"];
+                    $jsonPhoto["url"] = str_replace("\\", "/", $photos["url"]);
+                    $jsonPhoto["name"] = $photos["name"];
+                    array_push($jsonPhotos, $jsonPhoto);
+                }
+                $json["articles"] = $jsonPhotos;
+                $json["nextStart"] = $start + 10;
+                echo json_encode($json);
+            } else {
+                $json["error"] = "nothing";
+                echo json_encode($json);
             }
         }
     }
@@ -88,19 +120,16 @@ class ApiController extends Controller {
         }
     }
 
-    public function listeflashback($start = null) {
-        $this->getListe("flashabck", $start);
-    }
-
-    public function listeAlbum($start = null) {
-        $this->getListe("album", $start);
-    }
-
-    private function getListe($type, $start) {
+    public function album($page = null, $start = null) {
         header('Content-type: text/plain');
         header("Access-Control-Allow-Origin: *");
-        if ($start != null) {
+        $this->getListe("album", $start, $page);
+    }
+
+    private function getListe($type, $start, $page) {
+        if ($start != null && $page != null && $page == "page") {
             $json = array();
+            $jsonListe = array();
             $constructor = null;
             $count = null;
 
@@ -130,36 +159,11 @@ class ApiController extends Controller {
                     setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
                     $array["date_debut"] = strftime("%A %d %B %Y");
                     $array["id"] = $elt["id"];
-                    array_push($json, $array);
+                    array_push($jsonListe, $array);
                 }
-                $json["articles"] = $json;
+                $json["articles"] = $jsonListe;
                 $json["nextStart"] = $start + $limit;
                 echo json_encode($json);
-            } else {
-                $json["error"] = "nothing";
-                echo json_encode($json);
-            }
-        }
-    }
-
-    public function listePhoto($idAlbum = null, $start = null) {
-        header('Content-type: text/plain');
-        header("Access-Control-Allow-Origin: *");
-        if ($start != null) {
-            $jsonPhotos = array();
-            $photoConstruct = new Photos();
-            $photoConstruct->setId_album($idAlbum);
-            $count = $photoConstruct->countPhotosByIdAlbum();
-            if ($count > 0 && ($start == 0 || $count - $start > 0)) {
-                foreach ($photoConstruct->getAllPhotosByPageAndIdAlbum($start) as $photos) {
-                    $jsonPhoto["id"] = $photos["id"];
-                    $jsonPhoto["url"] = str_replace("\\", "/", $photos["url"]);
-                    $jsonPhoto["name"] = $photos["name"];
-                    array_push($jsonPhotos, $jsonPhoto);
-                }
-                $jsonPhotos["articles"] = $jsonPhotos;
-                $jsonPhotos["nextStart"] = $start + 10;
-                echo json_encode($jsonPhotos);
             } else {
                 $json["error"] = "nothing";
                 echo json_encode($json);
