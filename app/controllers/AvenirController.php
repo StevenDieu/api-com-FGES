101d23ac290b035de6ca71384c09443e34d8184d
MathieuIpii <math_6659@hotmail.com>
2016-11-17 14:04:27 +0100

    depart proj
    coucou stevee

--- app/controllers/AvenirController.php
+++ app/controllers/AvenirController.php
@@ -3,10 +3,46 @@
 class AvenirController extends Controller {
 
     private $dirView = 'avenir';
+    private $error = null;
+    private $avenirReturn = null;
+    private $success = null;
     
     public function index() {
         $this->render($this->dirView . '/index', array(
             'title' => 'A venir'
         ));
     }
+    
+    public function creation(){
+        $this->render($this->dirView . '/creation', array(
+            'title' => 'Création "A venir"',
+            'error' => $this->error,
+            'success' => $this->success,
+            'flashback' => $this->avenirReturn,
+            'nextId' => 1, // nextId
+            'froala' => true
+        ));        
+    }
+    
+    public function modification($id = null, $created = false) {
+        $this->render($this->dirView . '/modification', array(
+            'title' => 'Modification "A venir"',
+            'error' => $this->error,
+            'success' => $this->success,
+            //'arrayJs' => $arrayJs,
+            //'flashbacks' => $flashbacks,
+            //'flashback' => $flashback,
+            'froala' => true //$froala
+        ));        
+    }    
+    
+    public function suppression(){
+        $this->render($this->dirView . '/suppression', array(
+            'title' => 'Suppresion "A venir"',
+            'error' => $this->error,
+            'success' => $this->success,
+            'avenir' => null //$flashbacks
+        ));
+    }
+    
 }
